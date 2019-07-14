c211ea2f6220e277490f8407d3173380fd18f726
Stefanos <stefanosu422@gmail.com>
2019-07-14 13:20:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+const findMatching = (arr, name) => { 
+    return arr.filter(function(driverName) {
+    return driverName.toLowerCase() === name.toLowerCase();
+    
+    })
+}
+
+
+const fuzzyMatch = (arr, someName) => {
+    let nameLength = someName.length 
+    return arr.filter((driverName) => {
+        return driverName.slice(0, nameLength) === someName
+    })
+}
+
+
+const matchName = (arr, name) => {
+    return arr.filter(function(driver){
+        return driver.name.toLowerCase() === name.toLowerCase()
+    })
+}
+
